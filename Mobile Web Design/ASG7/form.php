4ef056ae7d0aa3b79c1c607bea4fdc760bfc6cc4
Ernesto B. Delacruz, Jr <ernesto.b.delacruz.jr@gmail.com>
2016-11-20 14:26:34 -0500

    HTTP to HTTPS to fix jQuery Mobile

--- Mobile Web Design/ASG7/form.php
+++ Mobile Web Design/ASG7/form.php
@@ -9,12 +9,12 @@
   <!-- Include meta tag to ensure proper rendering and touch zooming -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Include jQuery Mobile stylesheets -->
-  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"/>
+  <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
   <!-- Include the jQuery library -->
-  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
   <!-- Include the jQuery Mobile library -->
-  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-  
+  <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
   <link rel="stylesheet" href="themes/TinkerMonkey.min.css" />
   <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
 
@@ -22,39 +22,39 @@
 <body>
 
 <!--Homepage-->
-<div data-role="page" id="HomePage">   
+<div data-role="page" id="HomePage">
 
 		<!--Panel-->
-		<div data-role="popup" id="Info"> 			
-			
-				<p>Tinker Monkey is a game development production I made to share (I hope) any games I make, 
-					playable and in-progress. Once games are complete I plan on trying to release my products on 
-					Steam for sale in the future, with some games I plan to make free as well. I have gone as far 
-					as make a development email and also Twitter. Currently going to school and work full-time will 
+		<div data-role="popup" id="Info">
+
+				<p>Tinker Monkey is a game development production I made to share (I hope) any games I make,
+					playable and in-progress. Once games are complete I plan on trying to release my products on
+					Steam for sale in the future, with some games I plan to make free as well. I have gone as far
+					as make a development email and also Twitter. Currently going to school and work full-time will
 					make this extremely difficult so please bare with me!
 				</p>
-			
+
 		</div>
-		
+
 		<!--Header-->
 		<div data-role="header">
 			<a href="../ASG8/tour.html#Assignment7" rel="external" class="ui-btn ui-corner-all ui-icon-home ui-btn-icon-left ui-btn-icon-notext">Home</a>
 			<center><img src="images/TinkerLogo.jpg" alt="Tinker Monkey Productions" height="150" width="300"/></center>
 		</div>
-			
+
 		<!--Main-->
 		<div data-role="main" class="ui-content">
-		
-		
+
+
 			<!--Login/Register Page Buttons-->
 			<center>
 			<a href="register.php"class="ui-btn ui-btn-inline ui-btn-b ui-corner-all">New User</a>
 			<a href="login.php" class="ui-btn ui-btn-inline ui-btn-b ui-corner-all">Current User</a>
 			<a class="ui-btn ui-btn-inline ui-btn-b ui-corner-all">Guest</a>
 			</center>
-  			
-		
-		</div> 
-</div>	
+
+
+		</div>
+</div>
 </body>
 </html>
--- Mobile Web Design/ASG7/register.php
+++ Mobile Web Design/ASG7/register.php
@@ -9,12 +9,12 @@
   <!-- Include meta tag to ensure proper rendering and touch zooming -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Include jQuery Mobile stylesheets -->
-  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"/>
+  <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
   <!-- Include the jQuery library -->
-  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
   <!-- Include the jQuery Mobile library -->
-  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-  
+  <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
   <link rel="stylesheet" href="themes/TinkerMonkey.min.css" />
   <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
 
@@ -22,49 +22,49 @@
 <body>
 
 <!--Homepage-->
-<div data-role="page" id="HomePage">   
+<div data-role="page" id="HomePage">
 
 		<!--Panel-->
-		<div data-role="popup" id="Info"> 			
-			
-				<p>Tinker Monkey is a game development production I made to share (I hope) any games I make, 
-					playable and in-progress. Once games are complete I plan on trying to release my products on 
-					Steam for sale in the future, with some games I plan to make free as well. I have gone as far 
-					as make a development email and also Twitter. Currently going to school and work full-time will 
+		<div data-role="popup" id="Info">
+
+				<p>Tinker Monkey is a game development production I made to share (I hope) any games I make,
+					playable and in-progress. Once games are complete I plan on trying to release my products on
+					Steam for sale in the future, with some games I plan to make free as well. I have gone as far
+					as make a development email and also Twitter. Currently going to school and work full-time will
 					make this extremely difficult so please bare with me!
 				</p>
-			
+
 		</div>
-		
+
 		<!--Header-->
 		<div data-role="header">
 			<a href="../ASG8/tour.html#Assignment7" rel="external" class="ui-btn ui-corner-all ui-icon-home ui-btn-icon-left ui-btn-icon-notext">Home</a>
 			<center><img src="images/TinkerLogo.jpg" alt="Tinker Monkey Productions" height="150" width="300"/></center>
 		</div>
-			
+
 		<!--Main-->
 		<div data-role="main" class="ui-content">
-		
+
 			<!--Login/Register Page Buttons-->
 			<center>
 			<a class="ui-btn ui-btn-inline ui-corner-all">New User</a>
 			<a href="login.php" class="ui-btn ui-btn-inline ui-btn-b ui-corner-all">Current User</a>
 			<a class="ui-btn ui-btn-inline ui-btn-b ui-corner-all">Guest</a>
 			</center>
-  			
+
 			<!------------------------------------------------------------------------------->
-			
+
 			<!--Main/Register Form-->
 			<form id="Register" name="Register" method="POST" action="response.php">
-		
+
 			<!--Name-->
 			<div class="ui-field-contain">
-				First Name:<br>											
+				First Name:<br>
 				<input type="text" name="firstname" id="firstname">
-				Last Name:<br>										
+				Last Name:<br>
 				<input type="text" name="lastname" id="lastname"><br>
 			</div>
-			
+
 			<!--Gender-->
 			<div class="ui-field-contain">
 				<fieldset data-role="controlgroup">
@@ -72,11 +72,11 @@
 					<label for="male">Male</label>
 					<input type="radio" name="gender" id="male" value="male">
 					<label for="female">Female</label>
-					<input type="radio" name="gender" id="female" value="female"> 
+					<input type="radio" name="gender" id="female" value="female">
 					<label for="other">Other/Don't want to say</label>
-					<input type="radio" name="gender" id="other" value="other" checked="checked"> 
+					<input type="radio" name="gender" id="other" value="other" checked="checked">
 				</fieldset>
-			
+
 				<!--Date of birth-->
 				<fieldset data-role="controlgroup" data-type="horizontal" class="ui-field-contain">
 					<br>Date of birth:<br>
@@ -95,8 +95,8 @@
 							<option value="November">November</option>
 							<option value="December">December</option>
 						</select>
-				
-					<!--Day *Should I use day?*		
+
+					<!--Day *Should I use day?*
 					<label for="day">Day:</label>
 						<select name="day" id="day">
 							<option value="01">1</option>
@@ -131,8 +131,8 @@
 							<option value="30">30</option>
 							<option value="31">31</option>
 						</select>								-->
-		
-		
+
+
 					<!--Year Loop/PHP-->
 					<label for="year">Year:</label>
 						<select name="year" id="year">
@@ -144,22 +144,22 @@
 						}
 						?>
 						</select>
-					
+
 				</fieldset>
 			</div>
-			
+
 			<!--User/Password Request + Email-->
 			<div class="ui-field-contain">
-				Username:<br>										
+				Username:<br>
 				<input type="text" name="username" id="username"><br>
-				Password:<br>										
+				Password:<br>
 				<input type="PASSWORD" name="password" id="password">
-				Confirm Password:<br>										
+				Confirm Password:<br>
 				<input type="PASSWORD" name="confirmpass" id="confirmpass"><br>
-				Current Email Address:<br>										
+				Current Email Address:<br>
 				<input type="text" name="email" id="email">
 			</div>
-			
+
 			<!--Genre Survey-->
 			<div class="ui-field-contain">
 				<fieldset data-role="controlgroup">
@@ -169,32 +169,32 @@
 					<label for="adventure">Adventure</label>
 					<input type="checkbox" name="genre[]" id="adventure" value="Adventure">
 					<label for="rpg">RPG</label>
-					<input type="checkbox" name="genre[]" id="rpg" value="RPG"> 
+					<input type="checkbox" name="genre[]" id="rpg" value="RPG">
 					<label for="sim">Simulator</label>
 					<input type="checkbox" name="genre[]" id="sim" value="Simulator">
 					<label for="strat">Strategy</label>
 					<input type="checkbox" name="genre[]" id="strat" value="Strategy">
 					<label for="survival">Survival</label>
-					<input type="checkbox" name="genre[]" id="survival" value="Survival"> 
+					<input type="checkbox" name="genre[]" id="survival" value="Survival">
 					<label for="horror">Horror</label>
-					<input type="checkbox" name="genre[]" id="horror" value="Horror"> 
+					<input type="checkbox" name="genre[]" id="horror" value="Horror">
 					<label for="visualnovel">Visual Novel</label>
 					<input type="checkbox" name="genre[]" id="visualnovel" value="Visual Novel">
-				</fieldset>		
+				</fieldset>
 			</div>
 
 			<!--Submit/Reset Button-->
 			<input type="reset" value="Clear Page">
 			<input type="submit" name="submit" value="Register" />
 			</form>
-			
+
 			<!--Footer-->
 			<div data-role="footer">
 				<h3><a href="#Info" data-rel="popup" class="ui-btn ui-mini ui-btn-inline ui-shadow ui-corner-all ui-icon-info ui-btn-icon-notext">Info</a>
 				<font color="red">Work In Progress</font>
 				<a href="#Info" data-rel="popup" class="ui-btn ui-mini ui-btn-inline ui-shadow ui-corner-all ui-icon-info ui-btn-icon-notext">Info</a></h3>
 			</div>
-			
-	</div>   
+
+	</div>
 </body>
 </html>
